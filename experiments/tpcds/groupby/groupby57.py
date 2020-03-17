ea588ababcf31d4465fe9aa64cf7c58aab2efc3d
Scott <qingzma@gmail.com>
2020-03-17 14:28:54 +0000

    remote notebook

--- experiments/tpcds/groupby/groupby57.py
+++ experiments/tpcds/groupby/groupby57.py
@@ -42,18 +42,18 @@ def run():
 def build_models(sqlExecutor):
     # 10k
     sqlExecutor.execute(
-        "create table ss40g_600k_tes_gg_vpu(ss_sales_price real, ss_sold_date_sk real) from '/data/tpcds/40G/ss_600k.csv' GROUP BY ss_store_sk method uniform size 600000", n_mdn_layer_node=8, b_one_hot_encoding=True, b_grid_search=False, device='cpu', b_use_gg=True, n_per_gg=20)
+        "create table ss40g_600k_tes_gg_cpu(ss_sales_price real, ss_sold_date_sk real) from '/data/tpcds/40G/ss_600k.csv' GROUP BY ss_store_sk method uniform size 600000", n_mdn_layer_node=8, b_one_hot_encoding=True, b_grid_search=True, device='cpu', b_use_gg=True, n_per_gg=20)
     # "create table ss40g_600k(ss_sales_price real, ss_sold_date_sk real) from '/data/tpcds/40G/ss_600k.csv' GROUP BY ss_store_sk method uniform size 600000")
     # "create table ss_600k(ss_quantity real, ss_sales_price real) from '/data/tpcds/40G/ss_600k.csv' GROUP BY ss_store_sk method uniform size 600000")
 
 
 def query(sqlExecutor):
     sqlExecutor.execute(
-        "select count(ss_sales_price)  from ss40g_600k_tes_gg_vpu where ss_sold_date_sk between 2451119  and 2451483   group by ss_store_sk", n_jobs=1)
+        "select count(ss_sales_price)  from ss40g_600k_tes_gg_cpu where ss_sold_date_sk between 2451119  and 2451483   group by ss_store_sk", n_jobs=1)
     sqlExecutor.execute(
-        "select sum(ss_sales_price)  from ss40g_600k_tes_gg_vpu where ss_sold_date_sk between 2451119  and 2451483   group by ss_store_sk", n_jobs=1)
+        "select sum(ss_sales_price)  from ss40g_600k_tes_gg_cpu where ss_sold_date_sk between 2451119  and 2451483   group by ss_store_sk", n_jobs=1)
     sqlExecutor.execute(
-        "select avg(ss_sales_price)  from ss40g_600k_tes_gg_vpu where ss_sold_date_sk between 2451119  and 2451483   group by ss_store_sk", n_jobs=1)
+        "select avg(ss_sales_price)  from ss40g_600k_tes_gg_cpu where ss_sold_date_sk between 2451119  and 2451483   group by ss_store_sk", n_jobs=1)
 
     # ("select count(ss_quantity)  from ss_600k where ss_sales_price between 1  and 20   group by ss_store_sk")
 
