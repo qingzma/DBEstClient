45f28943dc255b36b9d860a49508d927a6442549
Scott <qingzma@gmail.com>
2020-04-01 15:08:08 +0100

    prepare for multiple group by

--- dbestclient/executor/test.py
+++ dbestclient/executor/test.py
@@ -6,26 +6,30 @@
 from builtins import print
 
 import numpy as np
+import torch
+import torch.nn.functional as functional
 from qregpy import qreg
 
-from dbestclient.ml import mdn
+print(functional.one_hot(torch.tensor([[0,1],[0,2],[0,3]])))
 
-x = np.linspace(0, 10, 100)
-y = x ** 2 + 1
-x = x[:, np.newaxis]
-# print(x)
-# print(y)
-# plt.plot(x,y)
-# plt.show()
+# from dbestclient.ml import mdn
 
-reg1 = qreg.QReg(base_models=["linear", "polynomial"], verbose=False).fit(x, y)
-preds1 = reg1.predict([[1], [2]])
-print(preds1)
+# x = np.linspace(0, 10, 100)
+# y = x ** 2 + 1
+# x = x[:, np.newaxis]
+# # print(x)
+# # print(y)
+# # plt.plot(x,y)
+# # plt.show()
 
-# x=x.reshape(-1,1)
-reg2 = mdn.RegMdn(dim_input=1).fit(x, y, num_gaussians=4, num_epoch=400)
-# preds2 = reg2.predict([1, 2])
-preds2 = reg2.predict([[1], [2]])
-preds2 = reg2.predict([[1], [2]])
-print("preds2", preds2)
-preds2
+# reg1 = qreg.QReg(base_models=["linear", "polynomial"], verbose=False).fit(x, y)
+# preds1 = reg1.predict([[1], [2]])
+# print(preds1)
+
+# # x=x.reshape(-1,1)
+# reg2 = mdn.RegMdn(dim_input=1).fit(x, y, num_gaussians=4, num_epoch=400)
+# # preds2 = reg2.predict([1, 2])
+# preds2 = reg2.predict([[1], [2]])
+# preds2 = reg2.predict([[1], [2]])
+# print("preds2", preds2)
+# preds2
