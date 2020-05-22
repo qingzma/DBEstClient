53b2e65bbe31fd80633621f19d7cf66471c3ad0f
Scott <qingzma@gmail.com>
2020-05-22 08:57:25 +0100

    prepare for drop model SQL

--- experiments/tpcds/groupby/groupby501.py
+++ experiments/tpcds/groupby/groupby501.py
@@ -207,31 +207,35 @@ def query_workload(sqlExecutor, model_name, n_jobs):
 
 def build_501_groups(sqlExecutor):
     sqlExecutor.execute("set v='True'")
-    sqlExecutor.execute("set device='cpu'")
+    sqlExecutor.execute("set device='gpu'")
     sqlExecutor.execute("set encoder='binary'")
-    sqlExecutor.execute("set b_grid_search='False'")
+    sqlExecutor.execute("set b_grid_search='false'")
     sqlExecutor.execute("set b_print_to_screen='false'")
     sqlExecutor.execute("set csv_split_char='|'")
     sqlExecutor.execute("set batch_size=1000")
+    # sqlExecutor.execute("set table_header=" +
+    #                     "'ss_sold_date_sk|ss_sold_time_sk|ss_item_sk|ss_customer_sk|ss_cdemo_sk|ss_hdemo_sk|" +
+    #                     "ss_addr_sk|ss_store_sk|ss_promo_sk|ss_ticket_number|ss_quantity|ss_wholesale_cost|" +
+    #                     "ss_list_price|ss_sales_price|ss_ext_discount_amt|ss_ext_sales_price|" +
+    #                     "ss_ext_wholesale_cost|ss_ext_list_price|ss_ext_tax|ss_coupon_amt|ss_net_paid|" +
+    #                     "ss_net_paid_inc_tax|ss_net_profit|none'"
+    #                     )
     sqlExecutor.execute("set table_header=" +
-                        "'ss_sold_date_sk|ss_sold_time_sk|ss_item_sk|ss_customer_sk|ss_cdemo_sk|ss_hdemo_sk|" +
-                        "ss_addr_sk|ss_store_sk|ss_promo_sk|ss_ticket_number|ss_quantity|ss_wholesale_cost|" +
-                        "ss_list_price|ss_sales_price|ss_ext_discount_amt|ss_ext_sales_price|" +
-                        "ss_ext_wholesale_cost|ss_ext_list_price|ss_ext_tax|ss_coupon_amt|ss_net_paid|" +
-                        "ss_net_paid_inc_tax|ss_net_profit|none'"
-                        )
+                        "'ss_sold_date_sk|ss_store_sk|ss_sales_price'")
 
     sqlExecutor.execute("set n_mdn_layer_node=20")
     sqlExecutor.execute("set n_jobs=1")
-    sqlExecutor.execute("set n_hidden_layer=2")
+    sqlExecutor.execute("set n_hidden_layer=1")
     sqlExecutor.execute("set n_epoch=20")
+    sqlExecutor.execute("set n_gaussians_reg=3")
+    sqlExecutor.execute("set n_gaussians_density=20")
     # sqlExecutor.execute("set result2file='/home/u1796377/Desktop/hah.txt'")
     sqlExecutor.execute(
-        "create table ss1t_501_node20_hidden2(ss_sales_price real, ss_sold_date_sk real) from '/data/tpcds/1t/ss_5m.csv' GROUP BY ss_store_sk method uniform size  'num_of_points501.csv' ")  # num_of_points57.csv
+        "create table ss1t_10gpu(ss_sales_price real, ss_sold_date_sk real) from '/data/tpcds/1t/ss_10m_reduced.csv' GROUP BY ss_store_sk method uniform size  'num_points/num_of_points501.csv' ")  # num_of_points57.csv
     sqlExecutor.execute(
-        "select avg(ss_sales_price)  from ss1t_501_node20_hidden2 where   2451119  <=ss_sold_date_sk<= 2451483    group by ss_store_sk")
+        "select avg(ss_sales_price)  from ss1t_10gpu where   2451119  <=ss_sold_date_sk<= 2451483    group by ss_store_sk")
 
-    query_workload(sqlExecutor, "ss1t_501_node20_hidden2", 1)
+    query_workload(sqlExecutor, "ss1t_10gpu", 1)
 
 
 def build_501_groups_grid_search(sqlExecutor):
@@ -258,7 +262,7 @@ def build_501_groups_grid_search(sqlExecutor):
 
     # sqlExecutor.execute("set result2file='/home/u1796377/Desktop/hah.txt'")
     sqlExecutor.execute(
-        "create table ss1t_501_grid_search(ss_sales_price real, ss_sold_date_sk real) from '/data/tpcds/1t/ss_5m.csv' GROUP BY ss_store_sk method uniform size  'num_of_points501.csv' ")  # num_of_points57.csv
+        "create table ss1t_501_grid_search(ss_sales_price real, ss_sold_date_sk real) from '/data/tpcds/1t/ss_5m.csv' GROUP BY ss_store_sk method uniform size  'num_points/num_of_points501.csv' ")  # num_of_points57.csv
     sqlExecutor.execute(
         "select avg(ss_sales_price)  from ss1t_501_grid_search where   2451119  <=ss_sold_date_sk<= 2451483    group by ss_store_sk")
     query_workload(sqlExecutor, "ss1t_501_grid_search", 1)
@@ -267,31 +271,37 @@ def build_501_groups_grid_search(sqlExecutor):
 def run_501_gogs(sqlExecutor):
     # sqlExecutor.execute("set device='cpu'")
     sqlExecutor.execute("set b_print_to_screen='False'")
-    sqlExecutor.execute("set n_mdn_layer_node=10")
+    sqlExecutor.execute("set device='cpu'")
     sqlExecutor.execute("set n_jobs=1")
-    sqlExecutor.execute("set n_hidden_layer=2")
-    sqlExecutor.execute("set n_epoch=20")
-    sqlExecutor.execute("set b_grid_search='true'")
-    sqlExecutor.execute("set csv_split_char='|'")
-    sqlExecutor.execute("set table_header=" +
-                        "'ss_sold_date_sk|ss_sold_time_sk|ss_item_sk|ss_customer_sk|ss_cdemo_sk|ss_hdemo_sk|" +
-                        "ss_addr_sk|ss_store_sk|ss_promo_sk|ss_ticket_number|ss_quantity|ss_wholesale_cost|" +
-                        "ss_list_price|ss_sales_price|ss_ext_discount_amt|ss_ext_sales_price|" +
-                        "ss_ext_wholesale_cost|ss_ext_list_price|ss_ext_tax|ss_coupon_amt|ss_net_paid|" +
-                        "ss_net_paid_inc_tax|ss_net_profit|none'"
-                        )
 
+    sqlExecutor.execute("set b_grid_search='false'")
+    sqlExecutor.execute("set csv_split_char='|'")
+    # sqlExecutor.execute("set table_header=" +
+    #                     "'ss_sold_date_sk|ss_sold_time_sk|ss_item_sk|ss_customer_sk|ss_cdemo_sk|ss_hdemo_sk|" +
+    #                     "ss_addr_sk|ss_store_sk|ss_promo_sk|ss_ticket_number|ss_quantity|ss_wholesale_cost|" +
+    #                     "ss_list_price|ss_sales_price|ss_ext_discount_amt|ss_ext_sales_price|" +
+    #                     "ss_ext_wholesale_cost|ss_ext_list_price|ss_ext_tax|ss_coupon_amt|ss_net_paid|" +
+    #                     "ss_net_paid_inc_tax|ss_net_profit|none'"
+    #                     )
+    sqlExecutor.execute("set table_header=" +
+                        "'ss_sold_date_sk|ss_store_sk|ss_sales_price'")
+
+    sqlExecutor.execute("set n_mdn_layer_node=10")
     sqlExecutor.execute("set b_use_gg='true'")
-    sqlExecutor.execute("set n_per_gg=100")
+    sqlExecutor.execute("set n_per_gg=102")
+    sqlExecutor.execute("set n_hidden_layer=1")
+    sqlExecutor.execute("set n_epoch=20")
     sqlExecutor.execute("set b_grid_search='false'")
     sqlExecutor.execute("set n_gaussians_reg=3")
-    sqlExecutor.execute("set n_gaussians_density=20")
+    sqlExecutor.execute("set n_gaussians_density=15")
 
     # sqlExecutor.execute("set result2file='/home/u1796377/Desktop/hah.txt'")
     sqlExecutor.execute(
-        "create table ss1t_gogs_128(ss_sales_price real, ss_sold_date_sk real) from '/data/tpcds/1t/ss_5m.csv' GROUP BY ss_store_sk method uniform size  'num_points/num_of_points501.csv' ")  # num_of_points57.csv
-    sqlExecutor.execute(
-        "select avg(ss_sales_price)  from ss1t_gogs_128 where   2451119  <=ss_sold_date_sk<= 2451483    group by ss_store_sk")
+        "create table ss1t_gogs10m_128(ss_sales_price real, ss_sold_date_sk real) from '/data/tpcds/1t/ss_10m_reduced.csv' GROUP BY ss_store_sk method uniform size  'num_points/num_of_points501.csv' ")  # num_of_points57.csv
+    # sqlExecutor.execute(
+    #     "select avg(ss_sales_price)  from ss1t_gogs10m_128 where   2451119  <=ss_sold_date_sk<= 2451483    group by ss_store_sk")
+
+    query_workload(sqlExecutor, "ss1t_gogs_128", 1)
 
 
 if __name__ == "__main__":
