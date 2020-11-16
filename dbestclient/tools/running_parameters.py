84f874de0e989a6edd61d5c02dec4c2894a22801
Quincy Ma <qingzma@gmail.com>
2020-11-16 18:54:44 +0000

    remove none

--- dbestclient/tools/running_parameters.py
+++ dbestclient/tools/running_parameters.py
@@ -61,7 +61,7 @@ class DbestConfig:
         self.config = {
             # system-level configuration.
             # 'warehousedir': '/home/u1796377/Programs/dbestwarehouse',
-            'warehousedir': 'dbestwarehouse',
+            'warehousedir': 'DBEstClient/DBEstClient/dbestwarehouse',
             # 'warehousedir': '/home/runner/work/DBEstClient/DBEstClient/',
             "reg_type": "mdn",
             "density_type": "mdn",  # qreg
