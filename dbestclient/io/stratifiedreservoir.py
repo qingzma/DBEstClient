d65c3cf8b27946cbc1dd39e1457cb7e4aff6d41c
Quincy Ma <qingzma@gmail.com>
2020-11-24 00:16:45 +0000

    save stratified

--- dbestclient/io/stratifiedreservoir.py
+++ dbestclient/io/stratifiedreservoir.py
@@ -4,8 +4,13 @@
 # the University of Warwick
 # Q.Ma.2@warwick.ac.uk
 
+
+import subprocess
+import os
+from glob import glob
 from datetime import datetime
 from random import randint, shuffle
+import dill
 
 
 class StratifiedReservoir:
@@ -16,17 +21,22 @@ class StratifiedReservoir:
         self.header = None
         self.file_name = file_name
         self.file_header = file_header
-        self.relevant_header = None
+        # self.relevant_header = None
         self.relevant_header_idx = []
-        self.ft = {}
+        # self.ft_sample = {}
+        self.ft_table = {}
         self.data_categoricals = {}
         self.data_features = {}
         self.data_labels = {}
         self.sample = {}
         self.n_jobs = n_jobs
         self.capacity = capacity
+        self.gb_cols = None
+        self.equality_cols = None
+        self.feature_cols = None
+        self.label_cols = None
 
-    def make_sample(self, gb_cols: list, equality_cols: list, feature_cols: list, label_cols: list, split_char=',', b_shuffle=False):
+    def make_sample(self, gb_cols: list, equality_cols: list, feature_cols: list, label_cols: list, split_char=',', b_shuffle=False, b_fast=False, b_return_sample=False):
         print("Start making a sample as requested...")
         t1 = datetime.now()
         # pre-process the file header
@@ -34,6 +44,11 @@ class StratifiedReservoir:
             with open(self.file_name, 'r') as f:
                 self.file_header = f.readline()
         headers = self.file_header.split(split_char)
+
+        self.gb_cols = gb_cols
+        self.equality_cols = equality_cols
+        self.feature_cols = feature_cols
+        self.label_cols = label_cols
 
         categorical_cols = gb_cols+equality_cols if equality_cols is not None else gb_cols
         categorical_cols_idx = [headers.index(item)
@@ -57,6 +72,8 @@ class StratifiedReservoir:
             with open(self.file_name, 'r') as f:
                 for line in f:
                     cnt += 1
+                    if cnt % 1000000 == 0:
+                        print(f"processed {cnt} records.")
                     splits = line.split(split_char)
                     categoricals = [splits[i] for i in categorical_cols_idx]
                     if feature_cols is not None:
@@ -64,24 +81,87 @@ class StratifiedReservoir:
                     labels = [splits[i] for i in label_cols_idx]
                     key = list2key(categoricals)
                     if key in self.sample:
+                        self.ft_table[key] += 1
                         if len(self.sample[key]) < self.capacity:
                             self.sample[key].append(
                                 categoricals+features+labels if feature_cols is not None else categoricals+labels)
                         else:
-                            j = randint(0, cnt)
-                            if j < self.capacity:
-                                self.sample[key][j] = categoricals + features + \
-                                    labels if feature_cols is not None else categoricals+labels
+                            if not b_fast:  # naive simple algorithm
+                                j = randint(0, cnt)
+                                if j < self.capacity:
+                                    self.sample[key][j] = categoricals + features + \
+                                        labels if feature_cols is not None else categoricals+labels
+                            else:  # fast algorithm
+                                # need to provide an extra ft to store the frequency.
+                                pass
+
                     else:
+                        self.ft_table[key] = 1
                         self.sample[key] = [
                             categoricals+features+labels] if feature_cols is not None else [categoricals+labels]
+            if b_return_sample:
+                return self.sample, self.ft_table
 
         else:
-            pass
+            from multiprocessing import Pool as PoolCPU
+            print("parallel sampling is only available in linux.")
+            line_num = int(subprocess.check_output(
+                ['wc', '-l', self.file_name]).split()[0])
+            # print(line_num)
+            n_per_file = int(line_num/self.n_jobs)+1
 
-        # prepare the frequency table
-        for key in self.sample:
-            self.ft[key] = len(self.sample[key])
+            split_command = f"split -l {n_per_file} --numeric-suffixes {self.file_name} tmp_split_"
+            process = subprocess.Popen(
+                split_command.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+
+            files = []
+            for i in range(self.n_jobs):
+                files.append("tmp_split_"+str(i) if i >=
+                             10 else "tmp_split_0"+str(i))
+
+            pool = PoolCPU(processes=self.n_jobs)
+            results = []
+            fts = []
+            instances = []
+            for file in files:
+                i = pool.apply_async(
+                    StratifiedReservoir(file, file_header=self.file_header, n_jobs=1, capacity=self.capacity).make_sample, (gb_cols, equality_cols, feature_cols, label_cols, split_char, False, b_fast, True))
+                instances.append(i)
+
+            for i in instances:
+                result, ft = i.get()
+                results.append(result)
+                fts.append(ft)
+                # print("--"*20)
+                # print(result)
+
+            self.sample = results[0]
+            self.ft_table = fts[0]
+            for result, ft in zip(results[1:], fts[1:]):
+                for key in result:
+                    if key not in self.sample:
+                        self.sample[key] = result[key]
+                        self.ft_table[key] = ft[key]
+                    elif self.ft_table[key] + ft[key] <= self.capacity:
+                        self.sample[key] = self.sample[key]+result[key]
+                        self.ft_table[key] += ft[key]
+                    else:
+                        new_result = self.sample[key]+result[key]
+                        shuffle(new_result)
+                        self.sample[key] = new_result[:self.capacity]
+                        self.ft_table[key] += ft[key]
+
+            # delete temporary files
+            for file in files:
+                os.remove(file)
+            # delete temporary files
+            # for file in glob('./tmp_split_*'):
+            #     os.remove(file)
+
+        # # prepare the frequency table
+        # for key in self.sample:
+        #     self.ft[key] = len(self.sample[key])
 
         # shuffle
         if b_shuffle:
@@ -94,18 +174,22 @@ class StratifiedReservoir:
         self.data_labels = {}
         for key in self.sample:
             self.data_categoricals[key] = [[row[idx] for idx in range(0, len(
-                categoricals))] for row in self.sample[key]]
+                categorical_cols_idx))] for row in self.sample[key]]
             if feature_cols is not None:
-                self.data_features[key] = [[row[idx] for idx in range(len(categoricals), len(
-                    categoricals)+len(features))] for row in self.sample[key]]
-                self.data_labels[key] = [row[idx] for idx in range(len(categoricals)+len(features),
-                                                                   len(categoricals)+len(features) + len(labels)) for row in self.sample[key]]
+                self.data_features[key] = [[row[idx] for idx in range(len(categorical_cols_idx), len(
+                    categorical_cols_idx)+len(feature_cols_idx))] for row in self.sample[key]]
+                self.data_labels[key] = [row[idx] for idx in range(len(categorical_cols_idx)+len(feature_cols_idx),
+                                                                   len(categorical_cols_idx)+len(feature_cols_idx) + len(label_cols_idx)) for row in self.sample[key]]
             else:
-                self.data_labels[key] = [row[idx] for idx in range(len(categoricals),
-                                                                   len(categoricals) + len(labels)) for row in self.sample[key]]
+                self.data_labels[key] = [row[idx] for idx in range(len(categorical_cols_idx),
+                                                                   len(categorical_cols_idx) + len(label_cols_idx)) for row in self.sample[key]]
         self.sample = None
-        # for key in self.ft:
-        #     print(key, self.ft[key])
+
+        # cntt = 0
+        # for key in self.ft_table:
+        #     print(key, self.ft_table[key])
+        #     cntt += self.ft_table[key]
+        # print("cntt", cntt)
 
         # for key in self.ft:
         #     # print(key, self.ft[key])
@@ -117,14 +201,22 @@ class StratifiedReservoir:
 
         print(
             f"Finish making the sample, time cost is {(datetime.now()-t1).total_seconds():.4f} seconds.")
+        return self.data_categoricals, self.data_features, self.data_labels
 
     def get_categorical_features_label(self):
         return self.data_categoricals, self.data_features, self.data_labels
+
+    def get_ft(self):
+        return self.ft_table
 
     def erase(self):
         self.data_categoricals = None
         self.data_features = None
         self.data_labels = None
+
+    def serialize2file(self, file):
+        with open(file, 'wb') as f:
+            dill.dump(self, f)
 
 
 class Reservoir:
--- tests/unit/dbestclient/io/stratifiedreservoir.py
+++ tests/unit/dbestclient/io/stratifiedreservoir.py
@@ -12,16 +12,33 @@ class TestStratifiedReservoir(unittest.TestCase):
     '''
     '''
 
-    def test_headers(self):
+    def test_tpcds(self):
         sr = StratifiedReservoir("data/tpcds/40G/ss_600k.csv",
                                  file_header="ss_sold_date_sk|ss_sold_time_sk|ss_item_sk|ss_customer_sk|ss_cdemo_sk|ss_hdemo_sk|ss_addr_sk|ss_store_sk|ss_promo_sk|ss_ticket_number|ss_quantity|ss_wholesale_cost|ss_list_price|ss_sales_price|ss_ext_discount_amt|ss_ext_sales_price|ss_ext_wholesale_cost|ss_ext_list_price|ss_ext_tax|ss_coupon_amt|ss_net_paid|ss_net_paid_inc_tax|ss_net_profit|none",
                                  n_jobs=1, capacity=5)
         sr.make_sample(gb_cols=["ss_store_sk"], equality_cols=["ss_coupon_amt"], feature_cols=["ss_sold_date_sk", "ss_ext_wholesale_cost"],
                        label_cols=["ss_sales_price"], split_char='|')
+        ft = sr.get_ft()
+
+        cntt = 0
+        for key in ft:
+            # print(key, self.ft_table[key])
+            cntt += ft[key]
+        # print("cntt", cntt)
+        # print("predictions", predictions)
+        self.assertEqual(cntt, 1000)
+
+    def test_hw(self):
+        sr = StratifiedReservoir("../data/huawei/merged",
+                                 file_header="ts,apmac,accType,radioid,band,ssid,usermac,downSpeed,rssi,upLinkSpeed,downLinkSpeed,txDiscardRatio,latency,downBytes,upBytes,kpiCount,authTimeoutTimes,assoFailTimes,authFailTimes,dhcpFailTimes,assoSuccTimes,authSuccTimes,dhcpSuccTimes,dot1XSuccTimes,dot1XFailTimes,onlineSuccTimes,txDiscardFrames,txFrames,tenantId,siteId,siteName,directRegion,regionLevelOne,regionLevelTwo,regionLevelThree,regionLevelFour,regionLevelFive,regionLevelSix,regionLevelSeven,regionLevelEight,parentResId,acName,resId,apname,publicArea,vendor,duration,badCount,badTime,lowRssiCount,lowRssiDur,highLatencyCount,highLatencyDur,highDiscardCount,highDiscardDur,nonFiveGCount,nonFiveGDur,exception_flag,last_acc_rst,linkQuality,portal_succ_times,portal_fail_times,roam_succ_times,roam_fail_times",
+                                 n_jobs=16, capacity=100)
+        sr.make_sample(gb_cols=["ts"], equality_cols=["regionLevelEight", "ssid"], feature_cols=["downSpeed"],
+                       label_cols=["latency"], split_char=',')
 
         # print("predictions", predictions)
         self.assertTrue(1 == 1)
 
 
 if __name__ == "__main__":
-    TestStratifiedReservoir().test_headers()
+    # TestStratifiedReservoir().test_tpcds()
+    TestStratifiedReservoir().test_hw()
